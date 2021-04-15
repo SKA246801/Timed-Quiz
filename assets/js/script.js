049afa26f7981647fdb750933df501da30ca2210
Sebastian <sebastianalvarado246801@gmail.com>
2021-04-15 11:48:52 -0500

    finised product

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,88 @@
 const question = document.querySelector('.question');
-const choices = document.querySelectorAll('choice');
+const choices = document.querySelectorAll('.choice');
 const progressText = document.querySelector('#progressText');
+const timerEl = document.querySelector('.timer');
+const answersEl = document.querySelector('.answer-choices');
 
+let time = 45
 let currentQuestion = {}
 let acceptingAnswers = true
 let score = 0
 let questionCounter = 0
 
+const questions = [
+{question:"The sky is blue", choices:["True", "False"], answer: "True",},
+{question:"A rock is a vegetable", choices:["True", "False"], answer: "False",},
+{question:"Austin is a continent", choices:["True", "False"], answer: "False",},
+{question:"Coding is boring", choices:["True", "False"], answer: "False",},
+{question:"This quiz works", choices:["True", "False"], answer: "True",}]
 
-const questions = [{question:"The sky is blue", choice1: "True", choice2: "False", answer: 1,},
-{question:"A rock is a vegetable", choice1: "True", choice2: "False", answer: 2,},
-{question:"Austin is a continent", choice1: "True", choice2: "False", answer: 2,},
-{question:"Coding is boring", choice1: "True", choice2: "False", answer: 2,},
-{question:"This quiz works", choice1: "True", choice2: "False", answer: 1,}]
 
 let max = questions.length
-startGame = () => {
+
+function startTimer() {
+    
+    timerInterval = setInterval(function(){
+        timerEl.innerText = 'Timer: ' + time
+        time--
+        if (time === -2) {
+            clearInterval(timerInterval)
+            alert("Time's up, try again")
+            location.href = "./highscores.html"
+        }
+    }, 1000) 
+}
+
+function startGame () {
     questionCounter = 0
     score = 0
-    availableQuestions = [...questions]
+    getNewQuestion()
+    startTimer()
+}
+
+function getNewQuestion () {
+    
+    let questionNumber = questionCounter + 1
+    progressText.innerText = `Question ${questionNumber} of ${max}`
+
+    currentQuestion = questions[questionCounter] 
+    question.innerText = currentQuestion.question
+    answersEl.innerText = ''
+    currentQuestion.choices.forEach(function (choice){
+        var choiceBtn = document.createElement('button')
+        choiceBtn.innerText = choice
+        choiceBtn.setAttribute('value', choice)
+        choiceBtn.setAttribute('class', 'btn')
+        choiceBtn.addEventListener('click', answers)
+        answersEl.appendChild(choiceBtn)
+
+    })
+
+}
+
+function answers () {
+    console.log(this.value)
+    if (this.value === currentQuestion.answer){
+        score++
+        console.log('correct')
+    } else {
+        console.log('incorrect')
+    }
+    questionCounter++
+    
+    console.log(questionCounter)
+    if (questionCounter === 5) {
+        location.href = "end.html"
+        localStorage.setItem('mostRecentScore', score)
+
+        getNewQuestion()
+    }
     getNewQuestion()
 }
 
-getNewQuestion = () => {
-    if(availableQuestions.length === 0 || questionCounter > max ) {
-        localStorage.setItem('mostRecentScore, score')
+function endGame () {
 
-        return window.location.assign('/end.html')
-    }
-
-    questionCounter++
-
-    progressText.innerText = `Question ${questionCounter} of ${max}`
-    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
-    currentQuestion = availableQuestion = availableQuestions[questionsIndex]
-    question.innerText = currentQuestion.question
-
-    choices.forEach(choice => {
-        const number = choice.dataset['number']
-        choice.innerText = currentQuestion['choice' + number]
-    })
-
-    availableQuestions.splice(questionsIndex, 1)
-
-    acceptingAnswers = true
 }
-
-choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return
-
-        acceptingAnswers = false
-        const selectedChoice = e.target
-        const selectedAnswer = selectedChoice.dataset['number']
-
-        if(selectedAnswer === currentQuestion.answer) {
-            score++
-        }
-
-    })
-})
-
 incrementScore = num => {
     score += num
     scoreTect.innerText = score
